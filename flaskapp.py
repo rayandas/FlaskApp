6bc020305a0720418b1e3407a7e7d7791f8a8cc6
Rayan Das <rayandas91@gmail.com>
2019-11-27 16:25:53 +0530

    initial commit

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,6 @@
-from flask import Flask, escape, request
-from flask import render_template
+'''
+from flask import Flask
+from flask import render_template, url_for
 
 app = Flask(__name__)
 
@@ -18,16 +19,36 @@ posts = [
         'date_posted': 'Aug 2, 2019'
     }
 ]
+'''
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Rayan Das',
+        'title': 'Blog Post 1',
+        'content': 'PyCon India 2019',
+        'date_posted': 'Oct 20, 2019'
+    },
+    {
+        'author': 'Sayan Chowdhury',
+        'title': 'Blog Post 2',
+        'content': 'Rootconf.In 2019',
+        'date_posted': 'Aug 2, 2019'
+    }
+]
 
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return render_template('about.html', title='About')   
+    return render_template('about.html', title='About')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)    
+    app.run(debug=True)
