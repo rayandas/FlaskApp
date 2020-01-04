7028ac62d0a93ca319fb10b20a855cd6da682095
Rayan Das <rayandas91@gmail.com>
2020-01-04 16:56:03 +0530

    update models

--- flaskapp/models.py
+++ flaskapp/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from flaskapp import db
+from flaskapp import db, login_manager
+from flask_login import UserMixin
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
