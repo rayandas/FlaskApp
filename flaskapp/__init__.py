57424c7e35849983742a5e284173263a2f9c97e7
Rayan Das <rayandas91@gmail.com>
2020-02-09 17:39:03 +0530

    updated __init__.py

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskapp import routes
