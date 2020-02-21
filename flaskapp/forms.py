3cd5f1fb41930879a95252efdca3e47165e55921
Rayan Das <rayandas91@gmail.com>
2020-02-21 12:05:25 +0530

    added request reset and password reset form

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -18,12 +18,12 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(username=username.data).first()
 
         if user:
-            raise ValidationError('This username is taken. Please choose a different one')
+            raise ValidationError('This username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('This email is taken. Please choose a different one')    
+            raise ValidationError('This email is taken. Please choose a different one.')    
 
 
 class LoginForm(FlaskForm):
@@ -58,3 +58,20 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with this email. You must register first.')  
+    
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Reset Password')
